c09f7f40d59a7b54c7f2b3ab10217340ff3e4592
HHHHHHHHHHHHHHHHHHHHHCS <464962683@qq.com>
2017-05-08 11:04:01 +0800

    更改了UI和JSON

---
+++ Assets/Scripts/Game/LevelJsonManager.cs
@@ -0,0 +1,63 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Newtonsoft.Json;
+using System.IO;
+using System;
+
+public class LevelJsonManager : MyJsonManager
+{
+    private static LevelJsonManager _instance;
+
+
+    public static LevelJsonManager Instance
+    {
+        get
+        {
+            if (_instance == null)
+            {
+                _instance = new LevelJsonManager();
+#if UNITY_ANDROID
+                _instance.filePath = string.Format("{0}/{1}", Application.persistentDataPath, fileName);
+#elif UNITY_IPHONE
+_instance.filePath = string.Format("{0}/{1}", Application.persistentDataPath, fileName);
+#else
+                _instance.filePath = string.Format("{0}/{1}", Application.dataPath, fileName);
+#endif
+            }
+            return _instance;
+        }
+
+    }
+
+    protected const string fileName = "LevelInfo.json";
+
+
+
+    public override string Read()
+    {
+        return base.Read();
+    }
+
+
+    public List<LevelInfo> ToList()
+    {
+        return base.ToList<LevelInfo>();
+    }
+
+    public string Save(List<LevelInfo> list)
+    {
+        return base.Save(list);
+    }
+
+    public string UpdateData(int level, int star)
+    {
+        var list = ToList();
+        if (star > list[level].star)
+        {
+            list[level].star = star;
+        }
+
+        return Save(list);
+    }
+}
---
+++ Assets/Scripts/Select/SelectCtrl.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class SelectCtrl : MonoBehaviour
+{
+    [SerializeField]
+    private GameObject selectLevelPrefab;
+    [SerializeField]
+    private Button preButton;
+    [SerializeField]
+    private Button nextButton;
+
+    private void Awake()
+    {
+        Debug.Log(LevelJsonManager.Instance.Read());
+    }
+
+}
