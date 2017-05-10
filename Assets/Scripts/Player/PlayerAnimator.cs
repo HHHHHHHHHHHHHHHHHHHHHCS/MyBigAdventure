128c8fafe69aa959406cccfbedfdd4a75d2719f5
HHHHHHHHHHHHHHHHHHHHHCS <464962683@qq.com>
2017-05-10 17:41:55 +0800

    人物移动和动画

---
+++ Assets/Scripts/Player/PlayerAnimator.cs
@@ -0,0 +1,102 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerAnimator : MonoBehaviour
+{
+    public class AnimParameters
+    {
+        public const string jumpTrigger = "jumpTrigger";
+        public const string isRun = "isRun";
+        public const string isRight = "isRight";
+        public const string isDead = "isDead";
+
+        public const string player_Left_Jump = "Player_Left_Jump";
+        public const string player_Right_Jump = "Player_Right_Jump";
+    }
+
+    private static PlayerAnimator _instance;
+
+    public static PlayerAnimator Instance { get { return _instance; } }
+
+    private Animator anim;
+
+    public bool GetIsRight
+    {
+        get
+        {
+            return anim.GetBool(AnimParameters.isRight);
+        }
+    }
+
+    public bool GetIsDead
+    {
+        get
+        {
+            return anim.GetBool(AnimParameters.isDead);
+        }
+    }
+
+    public bool GetIsRun
+    {
+        get
+        {
+            return anim.GetBool(AnimParameters.isRun);
+        }
+    }
+
+    public bool GetIsJumping
+    {
+        get
+        {
+            return anim.GetCurrentAnimatorStateInfo(0).IsName(AnimParameters.player_Left_Jump)
+                || anim.GetCurrentAnimatorStateInfo(0).IsName(AnimParameters.player_Right_Jump);
+        }
+    }
+
+
+    void Awake()
+    {
+        _instance = this;
+        anim = GetComponent<Animator>();
+    }
+
+    public void TurnLeft()
+    {
+        if (GetIsRight)
+        {
+            anim.SetBool(AnimParameters.isRight, false);
+        }
+    }
+
+    public void TurnRight()
+    {
+        if (!GetIsRight)
+        {
+            anim.SetBool(AnimParameters.isRight, true);
+        }
+    }
+
+    public void SetIsRun(bool tf)
+    {
+        if (GetIsRun != tf)
+        {
+            anim.SetBool(AnimParameters.isRun, tf);
+        }
+    }
+
+    public void SetJumpTrigger()
+    {
+        anim.SetTrigger(AnimParameters.jumpTrigger);
+    }
+
+    public void SetDead()
+    {
+        anim.SetBool(AnimParameters.isDead, true);
+    }
+
+    private void OnDestroy()
+    {
+        _instance = null;
+    }
+}
---
+++ Assets/Scripts/Player/PlayerCtrl.cs
@@ -0,0 +1,130 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerCtrl : MonoBehaviour
+{
+    private static PlayerCtrl _instance;
+
+    public static PlayerCtrl Instance { get { return _instance; } }
+
+
+
+    private Rigidbody2D rigi;
+
+    private Transform foot;
+    [Header("可以踩的层")]
+    [SerializeField]
+    private LayerMask layer;
+    [SerializeField]
+    private float moveSpeed = 10;
+    [SerializeField]
+    private float jumpSpeed = 10;
+
+    private Vector3 footOffest = new Vector3(0f, -0.05f, 0f);
+
+
+    private bool isJumpAndMove;
+
+
+    void Awake()
+    {
+        _instance = this;
+        rigi = GetComponent<Rigidbody2D>();
+        foot = transform.Find("Foot");
+    }
+
+    private void FixedUpdate()
+    {
+        Move();
+    }
+
+    public bool GetIsFloat()
+    {
+
+        RaycastHit2D ray = Physics2D.Linecast(foot.position, foot.position + footOffest, layer);
+        return ray.collider == null;
+
+    }
+
+    void Move()
+    {
+        bool isfloat = GetIsFloat();
+        bool isJumping = PlayerAnimator.Instance.GetIsJumping;
+        float fixedTime = Time.fixedDeltaTime;
+        if (!isJumping && !isfloat)
+        {
+            bool jumpAndMove = false;
+            Vector3 endPos = transform.position;
+            if (Input.GetKey(KeyCode.A))
+            {
+                PlayerAnimator.Instance.TurnLeft();
+                PlayerAnimator.Instance.SetIsRun(true);
+                jumpAndMove = true;
+                endPos += Vector3.left * moveSpeed * Time.fixedDeltaTime;
+            }
+            else if (Input.GetKey(KeyCode.D))
+            {
+                PlayerAnimator.Instance.TurnRight();
+                PlayerAnimator.Instance.SetIsRun(true);
+                jumpAndMove = true;
+                endPos += Vector3.right * moveSpeed * Time.deltaTime;
+            }
+            else
+            {
+                PlayerAnimator.Instance.SetIsRun(false);
+            }
+            if (Input.GetKeyDown(KeyCode.Space))
+            {
+                if (jumpAndMove)
+                {
+                    isJumpAndMove = true;
+                }
+                else
+                {
+                    isJumpAndMove = false;
+                }
+                PlayerAnimator.Instance.SetIsRun(false);
+                PlayerAnimator.Instance.SetJumpTrigger();
+                endPos += Vector3.up * jumpSpeed * Time.deltaTime;
+            }
+            if (endPos != transform.position)
+            {
+                rigi.MovePosition(endPos);
+            }
+        }
+        else if (isfloat)
+        {
+            Vector3 endPos = transform.position;
+            if (isJumpAndMove)
+            {
+                if (PlayerAnimator.Instance.GetIsRight)
+                {
+                    endPos += Vector3.right * moveSpeed * Time.deltaTime / 2;
+                }
+                else
+                {
+                    endPos += Vector3.left * moveSpeed * Time.deltaTime / 2;
+                }
+            }
+            if (isJumping)
+            {
+                endPos += Vector3.up * jumpSpeed * Time.deltaTime;
+            }
+            else
+            {
+                endPos += Vector3.down * 2 * jumpSpeed * Time.deltaTime;
+            }
+            if (endPos != transform.position)
+            {
+                rigi.MovePosition(endPos);
+            }
+        }
+        else if (!isJumpAndMove)
+        {
+            isJumpAndMove = false;
+        }
+
+    }
+
+}
